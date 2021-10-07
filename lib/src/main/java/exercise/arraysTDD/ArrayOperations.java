0a24c70ce0f755db8164cad015a36e836ef4e35e
AstridK24 <91180318+AstridK24@users.noreply.github.com>
2021-10-07 14:39:06 +0200

    Abgabe

--- lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
+++ lib/src/main/java/exercise/arraysTDD/ArrayOperations.java
@@ -1,5 +1,10 @@
 package exercise.arraysTDD;
 
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Arrays;
+
 /**
  * Utility class which performs various operations with integer arrays.
  */
@@ -22,7 +27,7 @@ public class ArrayOperations {
      */
 
     public void print() {
-        
+    	System.out.print(numbers.length);
     }
 
     /**
@@ -30,57 +35,123 @@ public class ArrayOperations {
      * @see <a href="sorting algortihms">http://faculty.cs.niu.edu/~hutchins/csci241/sorting.htm</a>
      */
     public int[] sort() {
-       return null;
-    }
+		for (int i = 0; i < numbers.length; i++) {
+        for (int j = 1; j < numbers.length; j++) {
+            if(numbers[j]<numbers[j-1]){
+                int temp = numbers[j];
+                numbers[j] = numbers[j-1];
+                numbers[j-1] = temp;
+            }
+        }
+	}
+    return numbers;
+}
 
     /**
      * @return the sorted array in reverse order
      */
     public int[] revertSort() {
-        return null;
+    	for (int i = 0; i < numbers.length; i++) {
+            for (int j = 1; j < numbers.length; j++) {
+                if(numbers[j]>numbers[j-1]){
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j-1];
+                    numbers[j-1] = temp;
+                    int numbers2 = numbers[j];
+                }
+            }
+    	}
+        return numbers;
     }
+  
+
+   
+        
 
     /**
      * @return the unsorted array in reverted order.
      */
     public int[] reverted() {
-        return null;
-    }
+    	int[] revertedNumbers = numbers.clone(); 
+
+        for (int i = 0; i< numbers.length; i++)
+        {
+            revertedNumbers[(numbers.length-1) - i] = numbers[i];
+        }
+
+        return revertedNumbers;
+        
+}
+    
 
     /**
      * @param value which should be searched for.
      * @return true if the array contains the value, false otherwise.
      */
     public boolean contains(int value) {
-        return false;
+    	
+		boolean found = false;
+		
+		for (int i = 0; i< numbers.length; i++) {
+    		if (numbers[i] == value) {
+    			found = true;
+    		}
+    	} 
+		
+        return found;
     }
 
     /**
      * @return the average value of all elements summed up.
      */
     public double average() {
-        return 0.0;
-    }
+    	 double sum = sumValues(); 
+
+ 		sum = sum / numbers.length;
+     		
+         return sum ;
+     }
+    
 
     /**
      * @return the average value of all elements summed up, but without the highest and the lowest value.
      */
     public double trimmedMean() {
-        return 0.0;
+      	double max = maxValue();
+    	double min = minValue();
+    	double sum = sumValues(); 
+
+   		return (sum - min - max) / (numbers.length - 2);
+    		
     }
 
     /**
      * @return the max value of the array. In the array [1,9,3] max would be 9.
      */
     public int maxValue() {
-        return 0;
+    	
+    	int max= numbers[0];
+    	for (int i = 1; i < numbers.length; i++ ) {
+    		if (numbers[i] > max) {
+    			max= numbers[i];
+    		}
+    	}
+    	  	
+    	
+        return max;
     }
 
     /**
      * @return the min value of the array. In the array [1,9,3] min would be 1.
      */
     public int minValue() {
-        return 0;
+    	int min = numbers[0];
+    	for (int i = 0; i< numbers.length; i++) {
+    		if (numbers[i] < min) {
+    			min= numbers[i];
+    		}
+    	}
+        return min;
     }
 
     /* **********************************************************
@@ -89,6 +160,18 @@ public class ArrayOperations {
      * ***********************************************************/
 
     private int someHelper(int[] tmp) {
+    	
         return 1;
     }
+	
+	private double sumValues(){
+		double sum = 0;
+		
+   		for (int i = 0; i < numbers.length; i++) {
+   			sum = sum + numbers[i];
+   		}	
+		
+		return sum;
+	}
 }
+
